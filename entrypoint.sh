aebc333c6fcb3f6576f3fb83c48475688458f68f
Fabricio Bastian <Bastian_Fabricio@shure.com>
2019-12-27 20:30:12 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+GITHUB="https://api.github.com"
+VERSION=$1
+REPO=$2
+FILE=$3
+
+if [ "$VERSION" = "latest" ]; then
+  # Github should return the latest release first.
+  parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
+else
+  parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
+fi;
+
+asset_id=`curl -H "Accept: application/vnd.github.v3.raw" -s $GITHUB/repos/$REPO/releases | jq "$parser"`
+if [ "$asset_id" = "null" ]; then
+  echo "ERROR: Version $VERSION was not found! 1>&2"
+  exit 1
+fi;
+
+wget -q --header='Accept:application/octet-stream' \
+  $GITHUB/repos/$REPO/releases/assets/$asset_id \
+  -O $FILE
