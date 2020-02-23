db69ed77f95c6e838c5139bc5e7f5f58d955f176
Fabricio Bastian <fabriciobastian@gmail.com>
2020-02-23 12:20:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,17 +14,12 @@ else
   parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
 fi;
 
-token_auth=""
 if [ ! -z "$TOKEN" ]; then
-  echo "--- Using access token ---"
-  token_auth="-H \"Authorization: token $TOKEN\""
+  asset_id=`curl -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.v3.raw" -s https://$GITHUBAPI/repos/$REPO/releases | jq "$parser"`
 else
-  echo "--- Not using access token ---"
+  asset_id=`curl -H "Accept: application/vnd.github.v3.raw" -s https://$GITHUBAPI/repos/$REPO/releases | jq "$parser"`
 fi;
 
-asset_id=`curl $token_auth -H "Accept: application/vnd.github.v3.raw" -s https://$GITHUBAPI/repos/$REPO/releases | jq "$parser"`
-asset_test = `curl $token_auth -H "Accept: application/vnd.github.v3.raw" -s https://$GITHUBAPI/repos/$REPO/releases`
-echo "asset: $asset_test"
 if [ "$asset_id" = "null" ]; then
   echo "ERROR: Version $VERSION was not found! 1>&2"
   exit 1
