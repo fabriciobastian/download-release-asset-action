56222c49c72f5b4d3d5d554683ff28133281f564
Fabricio Bastian <fabriciobastian@gmail.com>
2020-02-23 11:54:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ if [ "$asset_id" = "null" ]; then
 fi;
 
 if [ ! -z "$TOKEN" ]; then
-  wget -q --header='Accept:application/octet-stream' https://$GITHUBAPI/repos/$REPO/releases/assets/$asset_id -O $FILE
-else
   wget -q --auth-no-challenge --header='Accept:application/octet-stream' https://$TOKEN:@$GITHUBAPI/repos/$REPO/releases/assets/$asset_id -O $FILE
+else
+  wget -q --header='Accept:application/octet-stream' https://$GITHUBAPI/repos/$REPO/releases/assets/$asset_id -O $FILE
 fi;
